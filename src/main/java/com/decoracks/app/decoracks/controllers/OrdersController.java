7d68514f9a370b58e22fc7423560f98c42593565
Carlos Vega <carlosvegasba@gmail.com>
2025-07-10 00:37:24 -0500

    configurando vista pedidos

--- src/main/java/com/decoracks/app/decoracks/controllers/OrdersController.java
+++ src/main/java/com/decoracks/app/decoracks/controllers/OrdersController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -105,7 +106,8 @@ public class OrdersController {
     }
 
     @GetMapping("")
-    public String listar() {
+    public String listar(Model model) {
+        model.addAttribute("productos", productoService.findAll());
         return "orders";
     }
 
